6f7ac26b1c1b3a00acb16c49c3d8643073ab8846
Ravi <rksnhes1999@gmail.com>
2022-12-13 11:24:16 +0530

    second

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,26 +9,39 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg==true)
+            price=300;
+        else
+            price=400;
     }
 
     public int getPrice(){
+
+
         return this.price;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        price+=80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg==true)
+            price+=70;
+        else
+            price+=120;
     }
 
     public void addTakeaway(){
         // your code goes here
+        price+=20;
     }
 
     public String getBill(){
         // your code goes here
+        bill = String.valueOf(price);
         return this.bill;
     }
 }
